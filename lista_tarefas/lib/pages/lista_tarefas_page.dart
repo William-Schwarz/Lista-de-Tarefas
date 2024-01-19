a06abf052d088819b2563ffb8bc27cc99645cd0a
William Schwarz <williamdanielschwarz@gmail.com>
2024-01-18 21:04:52 -0300

    first commit

--- lista_tarefas/lib/pages/lista_tarefas_page.dart
+++ lista_tarefas/lib/pages/lista_tarefas_page.dart
@@ -1,27 +1,44 @@
 import 'package:flutter/material.dart';
 
 class ListaTarefasPage extends StatelessWidget {
-  const ListaTarefasPage({super.key});
+  ListaTarefasPage({super.key});
+
+  final TextEditingController emailController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16),
-          child: TextField(
-            decoration: InputDecoration(
-              labelText: 'E-mail',
-              hintText: 'exemplo@gmail.com',
-              border: OutlineInputBorder(),
-            ),
-            keyboardType: TextInputType.emailAddress,
-            style: TextStyle(
-              fontSize: 20,
-            ),
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: emailController,
+                decoration: const InputDecoration(
+                  labelText: 'E-mail',
+                  hintText: 'exemplo@gmail.com',
+                  border: OutlineInputBorder(),
+                ),
+                keyboardType: TextInputType.emailAddress,
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+              ),
+              ElevatedButton(
+                onPressed: login,
+                child: const Text('Entrar'),
+              ),
+            ],
           ),
         ),
       ),
     );
+  }
+
+  void login() {
+    String text = emailController.text;
+    print(text);
   }
 }
